e4533c174a6cc3f1849df408f1d5b4302a2b0d3b
Josuecordova17 <66342200+Josuecordova17@users.noreply.github.com>
2021-11-04 09:29:27 -0600

    Add files via upload

--- subirvideos.py
+++ subirvideos.py
@@ -23,8 +23,9 @@ tm.sleep(2)
 pg.click(1119,650)#Apreta el boton den siguiente para que baje
 tm.sleep(2)
 pg.click(256,591) #No es de ninos 
-for i in range (3):
- pg.click(1119,650)#Siguiente 
+pg.click(1119,650)#Siguiente
+pg.click(1119,650)#Siguiente
+pg.click(1119,650)#Siguiente 
 # si es de irwing le da oculto caso contrario publico   
 tm.sleep(2)
 if ir:
@@ -32,11 +33,10 @@ if ir:
     print(0)
 else:
     pg.press('up')
-tm.sleep(3)
 pg.click(1119,650) # le da a subir
 #si es de irwing abre la pagina para meter el formulario de videos
 if ir:
-    web.open("http://192.168.0.8") #Abre pagina de videos 
+    web.open("http://192.168.1.8") #Abre pagina de videos 
     tm.sleep(5)
     pg.click(627,121) #Click en el nombre
     tm.sleep(1)
@@ -46,12 +46,12 @@ if ir:
     tm.sleep(2)
     pg.hotkey('ctrl','v')
     pg.click(636,158)#Click al boton de mandar info
-pg.click(708,741)#abre el explorador de archivos
+pg.click(661,738)#abre el explorador de archivos
 tm.sleep(4)
 pg.click(72,261)#Videos 
 pg.click(456,169)#Click al primer video
 pg.hotkey ('ctrl', 'x')#Corta el video
 pg.click(347,161)
 pg.click(347,161)#Se mete a la carperta de Subir videos 
-tm.sleep(2)
+tm.sleep(4)
 pg.hotkey ('ctrl', 'v')#Pega el video
